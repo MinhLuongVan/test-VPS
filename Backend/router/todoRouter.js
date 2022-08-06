4928540ecb3d861185baf67d77e064f783c399db
MinhLuongVan <100786152+MinhLuongVan@users.noreply.github.com>
2022-08-06 14:27:01 +0700

    Update todoRouter.js

--- Backend/router/todoRouter.js
+++ Backend/router/todoRouter.js
@@ -2,8 +2,7 @@ const express = require('express')
 const router = express.Router();
 const todoController = require('../controller/todosController');
 const middleware = require('../controller/middleware');
-router.get('/',middleware.verifyTokenAuth,todoController.getAllList);
-router.get('/td',middleware.verifyToken,todoController.getAllListOwner);
+router.get('/',middleware.verifyToken,todoController.getAllListOwner);
 router.post('/todo',middleware.verifyToken,todoController.createList);
 router.delete('/:id',middleware.verifyToken,todoController.deleteList);
 router.put('/:id',middleware.verifyToken,todoController.updateList);
