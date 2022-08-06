875dda3c99719c05efa2f4a16810172d5a843937
MinhLuongVan <100786152+MinhLuongVan@users.noreply.github.com>
2022-08-06 15:31:48 +0700

    Delete Backend directory

--- Backend/router/authRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { model } = require('mongoose');
-const router = express.Router();
-const authController = require('../controller/authController');
- router.post('/register',authController.registerUser);
- router.post('/login', authController.loginUser);
- module.exports = router;
